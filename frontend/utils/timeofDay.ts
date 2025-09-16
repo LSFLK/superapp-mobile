0b34db448fb3e112ed4ff08aa2f32b7f9b7142ff
manodya <manodya@wso2.com>
2025-09-16 18:00:32 +0530

    remove unnecessary files

--- frontend/utils/timeofDay.ts
+++
@@ -1,7 +0,0 @@
-  // Get time-based greeting
-  export const getTimeOfDay = () => {
-    const hour = new Date().getHours();
-    if (hour < 12) return "Morning";
-    if (hour < 17) return "Afternoon";
-    return "Evening";
-  };
