93cfa95f83d40a9d0ba221b8681a94ec4f5051e1
Wishula Jayathunga <wishula@wishula.local>
2025-09-19 12:31:19 +0530

    Add the correct configs

--- admin_portal/admin-page/src/index.js
+++ admin_portal/admin-page/src/index.js
@@ -17,21 +17,21 @@ root.render(
   <React.StrictMode>
 
   <AuthProvider
-  /* config={ {
+  config={ {
             signInRedirectURL: "https://a96477cc-362b-4509-95ad-fcdb6507c34a.e1-us-east-azure.choreoapps.dev",
             signOutRedirectURL: "https://a96477cc-362b-4509-95ad-fcdb6507c34a.e1-us-east-azure.choreoapps.dev",
             clientID: "Bj8K5_LUYGGGy8lvc2xtSvfbj3ca",
             baseUrl: "https://api.asgardeo.io/t/jayathunga",
             scope: [ "openid","profile" ]
-        } } */
+        } }
 
-     config={ {
+     /* config={ {
             signInRedirectURL: "http://localhost:3000",
             signOutRedirectURL: "http://localhost:3000",
             clientID: "lwGgwFMsB86wdJlPLHUmmteWZf0a",
             baseUrl: "https://api.asgardeo.io/t/jayathunga",
             scope: [ "openid","profile" ]
-        } }    
+        } }  */   
   >
       <App />
     </AuthProvider>
